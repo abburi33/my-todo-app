1fca00ced1d6bf381edf236637f245eb34ebba6c
abburi33 <abburisahithi33@gmail.com>
2023-04-07 13:16:44 +0530

    Create basic GUI without interactions

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
     """ Read a text file and return the list of
     to-do items.
     """
@@ -7,7 +10,12 @@ def get_todos(filepath="todos.txt"):
     return todos_local
 
 
-def write_todos(new_todos, filepath="todos.txt"):
+def write_todos(new_todos, filepath=FILEPATH):
     """ Write the to-do items list  in the text file."""
     with open(filepath, "w") as todoFile:
         todoFile.writelines(new_todos)
+
+
+if "__name__" == "__main__":
+    print(get_todos())
+
